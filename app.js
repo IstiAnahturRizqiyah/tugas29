aafe087cc04fdb2521a735b509b1899d7789e404
isti <istianahturrizqiyah1306@gmail.com>
2023-11-03 10:04:48 +0700

    commit pertama

--- app.js
+++ app.js
@@ -1,45 +1,58 @@
-const http = require('http');
+const express = require('express');
+const path = require('path');
+const app = express();
 const port = 3000;
-const fs = require ("fs")
+const fs = require('fs');
+const contacts = require(`./contacts.json`);
 
-http
-    .createServer((req,res) => {
-        res.writeHead(200, {
-            'Content-Type': 'text/html',
-        });
-        const url = req.url;
-        if(url==='/about'){
-            fs.readFile('./about.html',(err,data) => {
-                if(err) {
-                    res.writeHead(484);
-                    res.write('Error : page not found');
-                } else {
-                    res.write(data)
-                }
-                res.end();
-            })
-        } else if(url=== '/contact'){
-            fs.readFile('./contact.html',(err,data) => {
-                if(err){
-                    res.writeHead(484);
-                    res.write('Error : page not found');
-                } else {
-                    res.write(data)
-                }
-                res.end();
-            })
-        } else {
-            fs.readFile('./index.html',(err,data) => {
-                if(err){
-                    res.writeHead(484);
-                    res.write('Error : page not found');
-                } else {
-                    res.write(data)
-                }
-                res.end();
-            })
+
+//information using EJS
+app.set('view engine', 'ejs');
+
+// Tentukan direktori views
+app.set('views', __dirname + '/views');
+
+app.get('/', (req, res) => {
+  // Mengirimkan halaman HTML utama
+    //res.sendFile("./index.html", { root: __dirname }); 
+    res.render('index', {nama: 'AKW', title:'WebServer EJS'}); 
+}); 
+
+
+app.get('/about', (req, res) => {
+  // Mengirimkan halaman "about.html"
+  // res.sendFile("./about.html", { root: __dirname }); 
+    res.render("about", {nama : "Isti Anahatur Rizqiyah"}); 
+}); 
+
+app.get('/contact', (req, res) => {
+  // Mengirimkan halaman "contact.html"
+  //res.sendFile("./contact.html", { root: __dirname }); 
+     // Route untuk halaman Contact
+    
+      // Baca data kontak dari file JSON
+        fs.readFile('contacts.json', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Terjadi kesalahan');
         }
-    })
-        .listen(3000,() => {
-            console.log('Server is running on port 3000 ...'); 
+        const contacts = JSON.parse(data);
+        // Render halaman Contact dengan data kontak
+        res.render('contact', { contacts });
+        });
+    });
+  
+app.get('/product/:id', (req, res) => {
+  // Lakukan sesuatu dengan productId dan category, misalnya mencari produk berdasarkan ID dan kategori.
+  res.send('product : '+ req.params.id +' kategori  : '+ req.query.kategori);
 });
+
+app.use('/', (req,res) => {
+    res.status(404)
+    res.send('page not found :  404')
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
+
